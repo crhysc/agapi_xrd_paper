2e376f88d67f88a78c7440eb5c1657a1c9121f0f
Charles Campbell <crc00042@mix.wvu.edu>
2026-03-31 22:34:46 -0400

    generate all figures

---
+++ generate_figures.sh
@@ -0,0 +1,9 @@
+echo "plot_refinement_mae.py"
+python plot_refinement_mae.py
+echo "plot_refinement_jsd_18panel.py"
+python plot_refinement_jsd_18panel.py
+echo "plot_match_rate_crystal_systems.py"
+python plot_match_rate_crystal_systems.py
+echo "stoich"
+bash stoich.sh
+
