4cd6000a00569de58a36195392ac55b480466afb
C. Rhys Campbell <149001340+crhysc@users.noreply.github.com>
2026-04-09 19:38:11 -0400

    Create analyse_filtered_rruff_runner.sh

---
+++ analyse_filtered_rruff_runner.sh
@@ -0,0 +1,11 @@
+python analyse_filtered_rruff.py \
+  --input runs/no_refinement/results.csv \
+  --output-dir runs/no_refinement/analysis
+
+python analyse_filtered_rruff.py \
+  --input runs/bmgn/results.csv \
+  --output-dir runs/bmgn/analysis
+
+python analyse_filtered_rruff.py \
+  --input runs/gsas2/results.csv \
+  --output-dir runs/gsas2/analysis
