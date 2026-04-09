83d763a5d2716e8079a42e3ee9986d27ec75f62f
C. Rhys Campbell <149001340+crhysc@users.noreply.github.com>
2026-04-09 19:39:43 -0400

    Update analyse_filtered_rruff_runner.sh

--- analyse_filtered_rruff_runner.sh
+++ analyse_filtered_rruff_runner.sh
@@ -1,11 +1,11 @@
 python analyse_filtered_rruff.py \
-  --input runs/no_refinement/results.csv \
+  --input runs/no_refinement/no_refinement_results.csv \
   --output-dir runs/no_refinement/analysis
 
 python analyse_filtered_rruff.py \
-  --input runs/bmgn/results.csv \
+  --input runs/bmgn/bmgn_results.csv \
   --output-dir runs/bmgn/analysis
 
 python analyse_filtered_rruff.py \
-  --input runs/gsas2/results.csv \
+  --input runs/gsas2/gsas2_results.csv \
   --output-dir runs/gsas2/analysis
