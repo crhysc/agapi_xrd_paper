9b0be4957ec2c42a1230c4d87d629b86d9a4b041
Charles Campbell <crc00042@mix.wvu.edu>
2026-04-09 20:45:58 -0400

    max val 10

--- analyse_filtered_rruff_runner.sh
+++ analyse_filtered_rruff_runner.sh
@@ -1,11 +1,18 @@
 python analyse_filtered_rruff.py \
   --input runs/no_refinement/no_refinement_results.csv \
-  --output-dir runs/no_refinement/analysis
+  --output-dir runs/no_refinement/analysis \
+  --max-val 10
 
 python analyse_filtered_rruff.py \
   --input runs/bmgn/bmgn_results.csv \
-  --output-dir runs/bmgn/analysis
+  --output-dir runs/bmgn/analysis \
+  --max-val 10
 
 python analyse_filtered_rruff.py \
   --input runs/gsas2/gsas2_results.csv \
-  --output-dir runs/gsas2/analysis
+  --output-dir runs/gsas2/analysis \
+  --max-val 10
+
+
+
+
